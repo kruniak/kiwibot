50468ee3f3f57935b75d4b6e5a71a7aa89a419ac
Martino <kruniak@protonmail.com>
2022-07-25 12:19:50 +0200

    //

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@
 // const { exec } = require('child_process');
 require('dotenv').config();
 
-// TEMPORARY: sticker management
-const bot = require('../scripts/stickerManagerBot');
-bot.run();
+// // TEMPORARY: sticker management
+// const bot = require('../scripts/stickerManagerBot');
+// bot.run();
 
 // // serve assets
 // exec('npx http-server assets');
 
-// const bot = require('./kbot/bot');
+const bot = require('./kbot/bot');
 
-// bot.run();
+bot.run();
